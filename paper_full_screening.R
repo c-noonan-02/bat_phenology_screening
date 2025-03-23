5940ba87fa9147c8b7661ee2c253263085df301f
c-noonan-02 <charlottenoonan@outlook.com>
2025-03-23 17:20:50 +0000

    Reviewed and completed screening of all papers from the meta analysis.

--- paper_full_screening.R
+++ paper_full_screening.R
@@ -4,4 +4,4 @@ rm(list = ls())
 library(metRscreen)
 
 #open csv file of papers
-metRscreen(screen.file = normalizePath("./full_screening_data/bat_search_string_results.csv"))
+metRscreen(screen.file = normalizePath("./full_screening_data/bat_papers_list.csv"))
